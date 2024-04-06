8bc05a52bd7d3bbe4a3cd4ee404dc8f0cffccc90
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-04-06 14:12:40 -0600

    Re-write window class

--- window.py
+++ window.py
@@ -3,25 +3,22 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.__root_wg = Tk()
-        self.__root_wg.title = "root"
-        self.canvas = Canvas()
-        self.canvas.pack(expand=1)
-        self.is_running = False
-
-        self.__root_wg.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, width=width,
+                               height=height, background='gray75')
+        self.__canvas.pack(expand=1, fill=BOTH)
+        self.__isWindowRunning = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.__root_wg.update_idletasks()
-        self.__root_wg.update()
+        self.__root.update_idletasks()
+        self.__root.update()
 
     def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
+        self.__isWindowRunning = True
+        while self.__isWindowRunning:
             self.redraw()
 
     def close(self):
-        self.is_running = False
-
+        self.__isWindowRunning = False
