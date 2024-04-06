e2e3b679516500fb7a0feff2ee90087585b7f8eb
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-04-06 17:12:12 -0600

    Add Cell class to draw squares

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from window import Window, Line, Point
+from window import Window, Line, Point, Cell
 
 
 def main():
     window = Window(800, 600)
-    linea = Line(Point(10, 10), Point(20, 20))
+    celda = Cell(True, True, False, True, window)
+    celda_3 = Cell(False, True, True, True, window)
 
+    celda.draw(100, 300, 200, 400)
+    celda_3.draw(700, 300, 800, 400)
 
-    window.draw_line(linea, "red")
+    celda.draw_move(celda_3)
 
     window.wait_for_close()
--- window.py
+++ window.py
@@ -24,7 +24,7 @@ class Window:
     def close(self):
         self.__isWindowRunning = False
 
-    def draw_line(self, line, color):
+    def draw_line(self, line, color="black"):
         line.draw(self.__canvas, color)
 
 
@@ -36,19 +36,64 @@ class Point:
 
 class Line:
     def __init__(self, point_A, point_B):
-        self.__point_A = point_A
-        self.__point_B = point_B
+        self.point_A = point_A
+        self.point_B = point_B
 
-    def draw(self, canvas, color):
-        x1 = self.__point_A.x
-        y1 = self.__point_A.y
+    def draw(self, canvas, color="black"):
+        x1 = self.point_A.x
+        y1 = self.point_A.y
 
-        x2 = self.__point_B.x
-        y2 = self.__point_B.y
+        x2 = self.point_B.x
+        y2 = self.point_B.y
 
         canvas.create_line(x1, y1, x2, y2, fill=color, width=2)
 
-        self.__canvas.pack(expand=1, fill=BOTH)
+        canvas.pack(expand=1, fill=BOTH)
 
 
+class Cell:
+    def __init__(self, left_wall, top_wall, right_wall, bottom_wall, win):
+        self.has_left_wall = left_wall
+        self.has_top_wall = top_wall
+        self.has_right_wall = right_wall
+        self.has_bottom_wal = bottom_wall
+        self.__x1 = None
+        self.__x2 = None
+        self.__y1 = None
+        self.__y2 = None
+        self.__win = win
 
+    def draw(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+        if self.has_left_wall:
+            self.__win.draw_line(Line(Point(x1, y1), Point(x1, y2)))
+        if self.has_top_wall:
+            self.__win.draw_line(Line(Point(x1, y1), Point(x2, y1)))
+        if self.has_right_wall:
+            self.__win.draw_line(Line(Point(x2, y1), Point(x2, y2)))
+        if self.has_bottom_wal:
+            self.__win.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+
+    def draw_move(self, to_cell, undo=False):
+        own_middle_x = abs((self.__x2 - self.__x1) / 2) + self.__x1
+        own_middle_y = abs((self.__y2 - self.__y1) / 2) + self.__y1
+
+        own_middle = Point(own_middle_x, own_middle_y)
+
+        other_middle_x = abs(((to_cell.__x2 - to_cell.__x1) / 2) + to_cell.__x1)
+        other_middle_y = abs(((to_cell.__y2 - to_cell.__y1) / 2) + to_cell.__y1)
+
+        other_middle = Point(other_middle_x, other_middle_y)
+
+        middle_line = Line(own_middle, other_middle)
+
+        color = "red"
+
+        if undo:
+            color = "gray"
+
+        self.__win.draw_line(middle_line, color)
